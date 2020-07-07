07557cdacb15d0887fe12f241f65dd45d6b6231d
Yiyao Zhang <zhang815605562@gmail.com>
2020-07-07 10:50:32 -0400

    fixed: group name display issue.

--- js/itemsPage.js
+++ js/itemsPage.js
@@ -1,3 +1,8 @@
+var selectedElement = [];
+var selectedGroupName = null;
+var itemDict = {};
+var loginSuccess = false;
+
 $(document).ready(function() {
   console.log("in doc ready");
 
@@ -13,14 +18,152 @@ $(document).ready(function() {
     success: loadData
   });
 
-
+  $.ajax({
+    type: "GET",
+    url: `https://www.duotuan.ca/data/dgGroupDataOut.php?epoch=${epoch}`,
+    dataType: "json",
+    success: loadGroupData
+  });
 
 });
 
+function checkLogIn() {
+  let name = getCookie("login");
+  let pass = getCookie("pass");
+  $.ajax({
+    type: "GET",
+    url: `https://www.duotuan.ca/login/checkLogin.php?login=${name}&pass=${pass}`,
+    dataType: "json",
+    success: checkResult
+  });
+  if (loginSuccess) {
+    return;
+  } else {
+    window.location.href = "https://www.duotuan.ca/login";
+  }
+}
+
+function checkResult(data) {
+  if (data.success === "success") {
+    loginSuccess = true;
+  } else {
+    loginSuccess = false;
+  }
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function removeGroup() {
+  if (selectedGroupName == null) {
+    alert("No group selected!");
+    return;
+  }
+  $.ajax({
+    type: "GET",
+    url: `https://www.duotuan.ca/data/dgGroupDel.php?groupName=${encodeURI(selectedGroupName)}`,
+    dataType: "json",
+  });
+  alert("success");
+  location.reload();
+}
+
+function selectGroup(groupName) {
+  if (selectedGroupName == null) {
+    selectedGroupName = groupName;
+    $(`#${groupName}`).addClass("groupSelected");
+  } else {
+    $(`#${selectedGroupName}`).removeClass("groupSelected");
+    if (selectedGroupName === groupName) {
+      selectedGroupName = null;
+    } else {
+      selectedGroupName = groupName;
+      $(`#${groupName}`).addClass("groupSelected");
+    }
+  }
+}
+
+function highLight(id) {
+  if (selectedElement.includes(id)) {
+    let index = selectedElement.indexOf(id);
+    if (index > -1) {
+      selectedElement.splice(index , 1);
+    }
+    $(`#${id}`).removeClass("selected");
+  } else {
+    selectedElement.push(id);
+    $(`#${id}`).addClass("selected");
+  }
+}
+
+function clearHighLight() {
+  for (let id of selectedElement) {
+    $(`#${id}`).removeClass("selected");
+  }
+  selectedElement = [];
+}
+
+function submitGroup() {
+  if (selectedElement.length === 0) {
+    alert("No item selected!");
+    return;
+  }
+  for (let id of selectedElement) {
+    $.ajax({
+      type: "GET",
+      url: `https://www.duotuan.ca/data/dgGroupIn.php?name=${encodeURI(itemDict[id])}&groupName=${encodeURI($("#groupName").val())}`,
+      dataType: "json",
+    });
+  }
+  alert("submited!");
+  location.reload();
+}
+
+function loadGroupData(data) {
+  let groupItems = {};
+  let counter = 0;
+  for (let item of data.data) {
+    if (groupItems[item.groupName] == null) {
+      groupItems[item.groupName] = 1;
+      $("#groupData").append(
+          `
+          <div id="${item.groupName}" class="flexContainer ${counter++ % 2 === 1 ? "greyed" : ""}"><h3>${item.groupName}</h3></div>
+          `
+      );
+      $(document.getElementById(item.groupName)).click(function () {
+        selectGroup(item.groupName);
+      });
+    }
+    $(document.getElementById(item.groupName)).append(
+        `
+        <div class="card-body">
+          <p>网址：${item.primaryURL}</p>
+          <h4 class="item-text">${item.name}</h2>
+          <p>价格：${item.price}</p>
+          <p>销量：${item.salesRecord}</p>
+        </div>
+        `
+    );
+  }
+}
 
 function loadData(data) {
   let sessions = [];
   for (let item of data.data) {
+    itemDict[item.id] = item.name;
     if (!sessions.includes(item.session)) {
       let name = item.primaryURL;
       if (name.includes("duotuan")) {
@@ -63,7 +206,7 @@ function loadData(data) {
     }
     $(`#${item.session}`).append(
       `
-        <div class="card-body">
+        <div id="${item.id}" class="card-body" onclick="highLight(${item.id})">
           <h4 class="item-text">${item.name}</h2>
           <p>价格：${item.price}</p>
           <p>销量：${item.salesRecord}</p>
@@ -71,6 +214,7 @@ function loadData(data) {
         <hr>
         `
     );
+
   };
 }
 
